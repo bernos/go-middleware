9c35e4d00c1bea66ca288690619fee9279e6bd57
Brendan McMahon <bernos@gmail.com>
2017-04-28 15:47:38 +1000

    Dont render views if an error exists in the request context

--- viewmiddleware/viewmiddleware.go
+++ viewmiddleware/viewmiddleware.go
@@ -25,8 +25,9 @@ func RenderView(options ...func(*options)) middleware.Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			v := FromRequest(r)
+			e := errormiddleware.FromRequest(r)
 
-			if v != nil {
+			if v != nil && e == nil {
 				t := v.Template
 
 				if t == nil {
